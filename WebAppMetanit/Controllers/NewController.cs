3f8ac212c01f04abe3064b3dfeb1e96d41f1ea9a
evgen.fesuk <evgen.fesuk@gmail.com>
2019-02-16 20:00:38 +0200

    some fixes

---
+++ WebAppMetanit/Controllers/NewController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppMetanit.Controllers
+{
+    public class NewController : Controller
+    {
+        // GET: New
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Experimental page";
+            ViewBag.Caption = "Big letters";
+
+            return View();
+        }
+    }
+}
